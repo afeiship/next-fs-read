c1580cdd0f70d77e36e6107cec41ac9d91dd7e38
afeiship <1290657123@qq.com>
2020-11-21 21:48:18 +0800

    refactor: utf8 to const

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@
   var nx = global.nx || require('@jswork/next');
   var fs = require('fs');
   var iconv = require('iconv-lite');
-  var DEFAULT_OPTIONS = { charset: 'utf8', responseType: 'string' };
+  var UTF8 = 'utf8';
+  var DEFAULT_OPTIONS = { charset: UTF8, responseType: 'string' };
 
   nx.fsRead = function (inFilename, inOptions) {
     var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
     var charset = options.charset;
     var buf = fs.readFileSync(inFilename);
-    var response = charset === 'utf8' ? buf.toString() : iconv.decode(buf, charset);
+    var response = charset === UTF8 ? buf.toString() : iconv.decode(buf, charset);
     switch (options.responseType) {
       case 'json':
         return JSON.parse(response);
