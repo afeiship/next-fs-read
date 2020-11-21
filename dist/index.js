f164a9108bb7592c787db506a6b6be9f0dc1718a
afeiship <1290657123@qq.com>
2020-11-21 15:03:18 +0800

    feat: add responseType support

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
  * description: Read file with fs for next.
  * homepage: https://github.com/afeiship/next-fs-read
  * version: 1.0.0
- * date: 2020-11-21 12:14:37
+ * date: 2020-11-21 15:03:10
  * license: MIT
  */
 
@@ -12,13 +12,21 @@
   var nx = global.nx || require('@jswork/next');
   var fs = require('fs');
   var iconv = require('iconv-lite');
-  var DEFAULT_OPTIONS = { charset: 'utf-8' };
+  var DEFAULT_OPTIONS = { charset: 'utf-8', responseType: 'string' };
 
   nx.fsRead = function (inFilename, inOptions) {
     var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
     var charset = options.charset;
     var buf = fs.readFileSync(inFilename);
-    return charset === 'utf-8' ? buf.toString() : iconv.decode(buf, charset);
+    var response = charset === 'utf-8' ? buf.toString() : iconv.decode(buf, charset);
+    switch (options.responseType) {
+      case 'json':
+        return JSON.parse(response);
+      case 'buffer':
+        return buf;
+      default:
+        return response;
+    }
   };
 
   if (typeof module !== 'undefined' && module.exports) {
--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@
   var nx = global.nx || require('@jswork/next');
   var fs = require('fs');
   var iconv = require('iconv-lite');
-  var DEFAULT_OPTIONS = { charset: 'utf-8' };
+  var DEFAULT_OPTIONS = { charset: 'utf-8', responseType: 'string' };
 
   nx.fsRead = function (inFilename, inOptions) {
     var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
     var charset = options.charset;
     var buf = fs.readFileSync(inFilename);
-    return charset === 'utf-8' ? buf.toString() : iconv.decode(buf, charset);
+    var response = charset === 'utf-8' ? buf.toString() : iconv.decode(buf, charset);
+    switch (options.responseType) {
+      case 'json':
+        return JSON.parse(response);
+      case 'buffer':
+        return buf;
+      default:
+        return response;
+    }
   };
 
   if (typeof module !== 'undefined' && module.exports) {
