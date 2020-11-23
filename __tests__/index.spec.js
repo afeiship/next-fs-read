c467f2c94d272ed48a14d0c9c809c46d834c556e
afeiship <1290657123@qq.com>
2020-11-23 13:18:47 +0800

    refactor: add test fixtures

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -3,17 +3,17 @@
 
   describe('api.basic test', () => {
     test('nx.fsRead charset:gbk', function () {
-      const target = nx.fsRead('./__tests__/E201.lrc', { charset: 'utf8' });
+      const target = nx.fsRead('./__tests__/fixtures/E201.lrc', { charset: 'utf8' });
       expect(target.includes('第一单元')).toBe(true);
     });
 
     test('nx.fsRead response: array', () => {
-      const target = nx.fsRead('./__tests__/E201.lrc', { responseType: 'array' });
+      const target = nx.fsRead('./__tests__/fixtures/E201.lrc', { responseType: 'array' });
       expect(Array.isArray(target)).toBe(true);
     });
 
     test('nx.fsRead response: json', () => {
-      const target = nx.fsRead('./__tests__/res.json', { responseType: 'json' });
+      const target = nx.fsRead('./__tests__/fixtures/res.json', { responseType: 'json' });
       expect(target).toEqual({ username: 'afeiship', homepage: 'https://js.work' });
     });
   });
