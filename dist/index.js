793ad8a8eb3db3725bee41bd8d56f7a513c53ccd
afeiship <1290657123@qq.com>
2020-11-23 13:16:43 +0800

    Release 1.0.2

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@
  * name: @jswork/next-fs-read
  * description: Read file with fs for next.
  * homepage: https://github.com/afeiship/next-fs-read
- * version: 1.0.1
- * date: 2020-11-23 13:15:57
+ * version: 1.0.2
+ * date: 2020-11-23 13:16:36
  * license: MIT
  */
 
